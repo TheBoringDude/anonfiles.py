8b76958c7995de8d5e222f5d6757cbf73f5d84fa
TheBoringDude <iamcoderx@gmail.com>
2021-07-16 21:43:50 +0800

    fix: lint errors and missing deps for dev

--- anonfiles/anonfiles.py
+++ anonfiles/anonfiles.py
@@ -3,12 +3,13 @@ from .resp import ErrorResp, SuccessRespData
 import requests
 
 
+API_URL = "https://api.anonfiles.com"
+
+
 class AnonFiles:
     """
     AnonFiles.com file hosting
     """
-
-    API_URL = "https://api.anonfiles.com"
 
     def __init__(self, token: str = None) -> None:
         """
@@ -19,10 +20,10 @@ class AnonFiles:
 
         # URL Docs: https://anonfiles.com/docs/api
 
-        self.__api_upload_url = self.API_URL + "/upload"  # UPLOAD api
+        self.__api_upload_url = API_URL + "/upload"  # UPLOAD api
         if token:
             self.__api_upload_url += f"?token={token}"
-        self.__info_url = self.API_URL + "/v2/file/{id}/info"  # INFO api
+        self.__info_url = API_URL + "/v2/file/{id}/info"  # INFO api
 
     def upload(self, file: str) -> AnonSuccessResponse | AnonErrorResponse:
         """
@@ -74,7 +75,7 @@ class BayFiles(AnonFiles):
 class AnonSuccessResponse:
     def __init__(self, json: dict) -> None:
         self.status = json.get("status")
-        self.data = SuccessRespData(json.get("data"))
+        self.data = SuccessRespData(json["data"])
 
     def __repr__(self) -> str:
         return f"\nSuccessfully uploaded `{self.data.file.metadata.name}` => {self.data.file.url.short}"
@@ -83,7 +84,7 @@ class AnonSuccessResponse:
 class AnonErrorResponse:
     def __init__(self, json: dict) -> None:
         self.status = json.get("status")
-        self.error = ErrorResp(json.get("error"))
+        self.error = ErrorResp(json["error"])
 
     def __repr__(self) -> str:
         return f"\nUpload Failed | {self.error.code} - {self.error.message}"
